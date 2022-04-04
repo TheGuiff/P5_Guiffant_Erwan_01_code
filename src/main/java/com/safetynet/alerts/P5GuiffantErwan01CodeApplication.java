c043f15bbc7b66083f06c99f9cf3d2b42818b723
Erwan Guiffant <ergu@mac.com>
2022-04-04 09:22:30 +0200

    feat:creation

---
+++ src/main/java/com/safetynet/alerts/P5GuiffantErwan01CodeApplication.java
@@ -0,0 +1,13 @@
+package com.safetynet.alerts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P5GuiffantErwan01CodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P5GuiffantErwan01CodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/safetynet/alerts/P5GuiffantErwan01CodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safetynet.alerts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P5GuiffantErwan01CodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
